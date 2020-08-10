13db06b102e69d5739ef23226106ffa87ae6f078
Giao Le <chuvoibebong@gmail.com>
2020-08-10 19:33:57 +0700

    First commit was unable to calculate big number (above 15 digts). Because of my lack in calculating with number stored in array, I used BigInteger class.

--- src/SmallFactorials.java
+++ src/SmallFactorials.java
@@ -1,23 +1,25 @@
 /*
-* Created on Monday, August 10, 2020
-* Author : Giao Le
-* */
+ * Created on Monday, August 10, 2020
+ * Author : Giao Le
+ * */
 
+
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class SmallFactorials {
-    public static int factorials(int N) {
-        if (N > 1) {
-            return N * factorials(N - 1);
-        } else {
-            return 1;
+    public static BigInteger factorials(int N) {
+        BigInteger s = new BigInteger("1");
+        for (int i = 2; i <= N; i++) {
+            s = s.multiply(BigInteger.valueOf(i));
         }
+        return s;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int testCases = scanner.nextInt();
-        while(testCases-- > 0) {
+        while (testCases-- > 0) {
             System.out.println(factorials(scanner.nextInt()));
         }
     }
