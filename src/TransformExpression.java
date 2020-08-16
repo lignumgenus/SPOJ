117cbe883b5bfd7f36d0df4d5da4e3711ec4c89a
giaole <chuvoibebong@gmail.com>
2020-08-16 17:23:42 +0700

    It's took me about one and half hour to finish this basic problem ^_^

---
+++ src/TransformExpression.java
@@ -0,0 +1,43 @@
+/*
+* Created on : Sunday, August 16, 2020
+* Author : Giao le
+* */
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class TransformExpression {
+    public static String RPN(String expression) {
+        String rpn = "";
+        Stack stack = new Stack();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            switch (c) {
+                case '(':
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '^':
+                    stack.push(c);
+                    break;
+                case ')':
+                    rpn += stack.pop();
+                    break;
+                default:
+                    rpn += c;
+                    break;
+            }
+        }
+        return rpn;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        while (testCases-- > 0) {
+            System.out.println(RPN(scanner.next()));
+        }
+    }
+}
